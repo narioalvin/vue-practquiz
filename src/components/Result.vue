fabb7b5f7a30a317cf3c64e551d602ef246f87b4
Alvin Nario <59873164+narioalvin@users.noreply.github.com>
2020-06-19 16:56:13 +0800

    result page

---
+++ src/components/Result.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="result">
+    <img src="../assets/img/idea.svg" width="120" />
+    <h1 v-if="score === totalQuestions">Wow! Perfect Score! :)</h1>
+    <h1 v-else-if="score === totalQuestions - 1">Almost Perfect! :)</h1>
+    <h1 v-else-if="score === 0">Sad. But you can try again! :)</h1>
+    <h1 v-else>Awesome! :)</h1>
+    <h1>{{ score }}/{{ totalQuestions }}</h1>
+    <button class="primary" @click="tryAgain">
+      TRY AGAIN
+    </button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Result',
+  props: ['score', 'totalQuestions'],
+  mounted() {
+    const element = document.querySelector('.result');
+    element.style['-webkit-animation'] = 'animLeft .3s';
+  },
+  beforeRouteEnter(to, from, next) {
+    const items = to.params;
+    if (Object.keys(items).length === 0) next({ name: '' });
+    else next();
+  },
+  methods: {
+    tryAgain() {
+      this.$router.push({ name: 'Setup' });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
